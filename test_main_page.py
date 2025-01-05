a5745f85eeee0a1820fbca6bca94c43483768c19
Mikola <kolya_popov_86@tut.by>
2025-01-05 18:58:03 +0300

    'transitions_between_pages'

--- test_main_page.py
+++ test_main_page.py
@@ -10,12 +10,14 @@ from .pages.login_page import LoginPage
 #     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
 #     login_link.click()
 
-# def test_quest_can_go_to_login_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)  # инициализируем Page Object,
-#     # передаем в конструктор экземпляр драйвера и url адрес
-#     page.open()  # открываем страницу
-#     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+def test_quest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)  # инициализируем Page Object,
+    # передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 #
 #
 # def test_quest_should_see_login_link(browser):
